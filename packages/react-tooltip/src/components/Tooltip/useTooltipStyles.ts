205dad568ce38af41e57ffee92142058cab5b48b
Ben Howell <behowell@microsoft.com>
2021-04-08 05:10:23 -0700

    Fix tooltip styles for new makeStyles

--- packages/react-tooltip/src/components/Tooltip/useTooltipStyles.ts
+++ packages/react-tooltip/src/components/Tooltip/useTooltipStyles.ts
@@ -11,78 +11,75 @@ export const arrowSquareSize = 12 * Math.SQRT1_2;
 
 export const tooltipBorderRadius = (theme: Theme) => theme.global.borderRadius.medium;
 
+const startsWith = (str: string, start: string) => str.substring(0, start.length) === start;
+
 /**
  * Styles for the root slot
  */
-const useRootStyles = makeStyles<TooltipState>([
-  [
-    null,
-    theme => ({
-      display: 'inline-block',
-      padding: '5px 12px 7px 12px',
-      pointerEvents: 'none',
-      maxWidth: '240px',
-      fontFamily: theme.global.type.fontFamilies.base,
-      fontSize: theme.global.type.fontSizes.base[200],
-      lineHeight: theme.global.type.lineHeights.base[200],
-      borderRadius: tooltipBorderRadius(theme),
+const useStyles = makeStyles({
+  root: theme => ({
+    display: 'inline-block',
+    padding: '5px 12px 7px 12px',
+    pointerEvents: 'none',
+    maxWidth: '240px',
+    fontFamily: theme.global.type.fontFamilies.base,
+    fontSize: theme.global.type.fontSizes.base[200],
+    lineHeight: theme.global.type.lineHeights.base[200],
+    borderRadius: tooltipBorderRadius(theme),
 
-      background: theme.alias.color.neutral.neutralForeground2, // TODO should be neutralBackgroundInverted
-      color: theme.alias.color.neutral.neutralForegroundInverted,
+    background: theme.alias.color.neutral.neutralForeground2, // TODO should be neutralBackgroundInverted
+    color: theme.alias.color.neutral.neutralForegroundInverted,
 
-      filter: theme.alias.shadow.shadowFilter8,
-    }),
-  ],
+    filter: theme.alias.shadow.shadowFilter8,
+  }),
 
-  [
-    s => s.subtle,
-    theme => ({
-      background: theme.alias.color.neutral.neutralBackground1,
-      color: theme.alias.color.neutral.neutralForeground1,
-    }),
-  ],
-]);
+  // s => s.subtle
+  subtle: theme => ({
+    background: theme.alias.color.neutral.neutralBackground1,
+    color: theme.alias.color.neutral.neutralForeground1,
+  }),
 
-const startsWith = (str: string, start: string) => str.substring(0, start.length) === start;
+  arrow: theme => ({
+    position: 'absolute',
+    width: `${arrowSquareSize}px`,
+    height: `${arrowSquareSize}px`,
+    background: 'inherit',
+    visibility: 'hidden',
 
-const useArrowStyles = makeStyles<TooltipState>([
-  [
-    null,
-    theme => ({
+    ':before': {
+      content: '""',
       position: 'absolute',
-      width: `${arrowSquareSize}px`,
-      height: `${arrowSquareSize}px`,
+      width: 'inherit',
+      height: 'inherit',
       background: 'inherit',
-      visibility: 'hidden',
+      visibility: 'visible',
+      borderBottomRightRadius: theme.global.borderRadius.small,
+    },
+  }),
 
-      ':before': {
-        content: '""',
-        position: 'absolute',
-        width: 'inherit',
-        height: 'inherit',
-        background: 'inherit',
-        visibility: 'visible',
-        borderBottomRightRadius: theme.global.borderRadius.small,
-      },
-    }),
-  ],
-  [s => startsWith(s.placement, 'top'), { bottom: `-${arrowHeight}px`, ':before': { transform: 'rotate(45deg)' } }],
-  [s => startsWith(s.placement, 'bottom'), { top: `-${arrowHeight}px`, ':before': { transform: 'rotate(225deg)' } }],
-  [s => startsWith(s.placement, 'left'), { right: `-${arrowHeight}px`, ':before': { transform: 'rotate(315deg)' } }],
-  [s => startsWith(s.placement, 'right'), { left: `-${arrowHeight}px`, ':before': { transform: 'rotate(135deg)' } }],
-]);
+  arrowTop: { bottom: `-${arrowHeight}px`, ':before': { transform: 'rotate(45deg)' } },
+  arrowBottom: { top: `-${arrowHeight}px`, ':before': { transform: 'rotate(225deg)' } },
+  arrowLeft: { right: `-${arrowHeight}px`, ':before': { transform: 'rotate(315deg)' } },
+  arrowRight: { left: `-${arrowHeight}px`, ':before': { transform: 'rotate(135deg)' } },
+});
 
 /**
  * Apply styling to the Tooltip slots based on the state
  * {@docCategory Tooltip}
  */
 export const useTooltipStyles = (state: TooltipState): TooltipState => {
-  state.className = ax(useRootStyles(state), state.className);
+  const styles = useStyles();
 
-  const arrowClassName = useArrowStyles(state);
-  if (state.arrow) {
-    state.arrow.className = ax(arrowClassName, state.arrow.className);
-  }
+  state.className = ax(styles.root, state.subtle && styles.subtle, state.className);
+
+  state.arrow.className = ax(
+    styles.arrow,
+    startsWith(state.placement, 'top') && styles.arrowTop,
+    startsWith(state.placement, 'bottom') && styles.arrowBottom,
+    startsWith(state.placement, 'left') && styles.arrowLeft,
+    startsWith(state.placement, 'right') && styles.arrowRight,
+    state.arrow.className,
+  );
 
   return state;
 };
